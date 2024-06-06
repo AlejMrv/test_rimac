45c8cf01980f1e2258c31a2dc0c265d10787ed3c
alejmrv <alejmrv@LAPTOP-1P38V0QC>
2024-06-06 15:11:13 -0500

    test

--- model/gcp_utils/cloud_functions.py
+++ model/gcp_utils/cloud_functions.py
@@ -3,7 +3,7 @@ from google.api_core.exceptions import NotFound, Conflict
 import os
 
 
-project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 PATH_CREDENTIALS = os.path.join('gcp_credential.json')
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = PATH_CREDENTIALS
 
