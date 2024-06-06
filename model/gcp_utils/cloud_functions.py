0aa9df103575601e10422e9254bd02bda8951cab
alejmrv <alejmrv@LAPTOP-1P38V0QC>
2024-06-06 14:55:36 -0500

    test

--- model/gcp_utils/cloud_functions.py
+++ model/gcp_utils/cloud_functions.py
@@ -5,7 +5,7 @@ import os
 
 #project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 #PATH_CREDENTIALS = os.path.join('gcp_credential.json')
-credentials_path = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+PATH_CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = PATH_CREDENTIALS
 
 storage_client = storage.Client()
